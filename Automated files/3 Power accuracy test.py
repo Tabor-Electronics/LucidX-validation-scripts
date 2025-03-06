35c9f3cf356912ff664bc9366a0ca0302e80f6b0
shivani <shivani@taborelec.com>
2025-03-06 18:12:54 +0530

    Automated script for 3 power accuracy test second commit

--- Automated files/3 Power accuracy test.py
+++ Automated files/3 Power accuracy test.py
@@ -1,54 +1,62 @@
-print("Test description :- This test gives out the value frequency and  power and compare the given threshold to conclude the result for accuracy of power  given frequency")
-print("Steps:- \n1) In this script we are using spectrum analyzer as a measuring device.\n2) We will set a center  frequency and  span to verify the signal power in dBm")
+print("<DESCRIPTION>Test description :- This test gives out the value frequency and  power and compare the given threshold to conclude the result for accuracy of power  given frequency\nSteps:- \n1) In this script we are using spectrum analyzer as a measuring device.\n2) We will set a center  frequency and  span to verify the signal power in dBm</DESCRIPTION>")
 ###########################
 ###START OF SCRIPT###
-
-from SourceFiles.functions_v1 import Lucid_functions,SignalGeneration
+# SECTION 0 - Import the required libraries
+from SourceFiles.functions_v1 import Lucid_functions, SignalGeneration
 from SourceFiles.spectrum_analyser_functions import spectrum_methods
 from SourceFiles import config
-from SourceFiles.lucid_cmd import LucidCmd
+from SourceFiles.for_the_gui import DevicePrint
 
-#Establishing connection with LUCIDX
+# SECTION 1- Connect LUCIDX, create object of Device print, connect to measuring device
 handle = config.handle
-Lucid_functions.reset(handle)
+Lucid_functions.reset(handle)  # Establishing connection with LUCIDX
 
-print("Start spectrum analyzer")
+devicePrintCmd = DevicePrint()
+devicePrintResp = DevicePrint(print_type=1)
+
+devicePrintCmd.msg_user.set('Connecting to the spectrum analyzer')
+devicePrintCmd.Print()
 # commands for spectrum analyzer
 if config.spectrum:
-    device_address = 'TCPIP::{0}::{1}::SOCKET'.format(config.spectrum_ip_address_india,config.port)  # Spectrum analyzer TCPIP  address
-    spectrum_analyzer,status = spectrum_methods.reset(device_address)
-    spectrum_methods.set_reference_power(config.power_default + 5, spectrum_analyzer)  # step 1) set reference power level on spectrum
-    spectrum_methods.set_span_freq(200,spectrum_analyzer)
-    
-#Global Parameters
-frequency = config.frequency_default # frequency in MHz
-power = config.power_list # list of power for testing
+    spectrum_analyzer, status = spectrum_methods.reset(config)
+    if status:
+        spectrum_methods.set_reference_power(config.power_default + 5,
+                                             spectrum_analyzer)  # step 1) set reference power level on spectrum
+        spectrum_methods.set_span_freq(200, spectrum_analyzer)  # step 2) set span to 200MHz
+
+# Global Parameters
+frequency = config.frequency_default  # frequency in MHz
+power = config.power_list  # list of power for testing
 
 for powe in config.power_list:
     # continous wave generation
-    freq_query,power_query = SignalGeneration.continous_wave_generation(frequency, powe) # continous signal generation
-    print(f"Frequency = {freq_query}, Power ={power_query}")
-    
+    freq_query, power_query = SignalGeneration.continous_wave_generation(frequency, powe)  # continous signal generation
+    # print(f"Frequency = {freq_query}, Power ={power_query}")
+    devicePrintCmd.msg_gui.set(f'freq={freq_query}::p0.00::n0.00,pow={power_query}::p0.00::n0.00')
+    devicePrintCmd.Print()
     # commands for spectrum analyzer
-    if config.spectrum:# spectrum commands for automation
+    if config.spectrum:  # spectrum commands for automation
         cf = frequency  # center frequency on measuring device
-        spectrum_methods.set_reference_power(config.power_default + 5, spectrum_analyzer)
-        spectrum_methods.set_centre_frequency(cf, spectrum_analyzer) # set center frequency on spectrum
-        freq_out,power_max = spectrum_methods.set_marker(spectrum_analyzer) #  Read marker x (frequency) and y (power)
-        error_value=abs(float(freq_out) - cf)  # Calculating difference between input and output frequency
-        power_error = abs(float(power_max)-config.power_default)  # Calculating difference between input and output power
-        frequency_th = 0.1 #  frequency threshold in term of percentage of input frequency
-        power_th = 1  #  power threshold in dBm
-        if (error_value <( frequency_th*cf)) and (power_error <power_th): # Condition to conclude the test result
-            print("Test pass for Frequency = {0} MHz".format(cf))
+        spectrum_methods.set_reference_power(powe + 5, spectrum_analyzer)
+        spectrum_methods.set_centre_frequency(cf, spectrum_analyzer)  # set center frequency on spectrum
+        freq_out, power_max = spectrum_methods.set_marker(spectrum_analyzer)  # Read marker x (frequency) and y (power)
+        error_value = abs(float(freq_out) - cf)  # Calculating difference between input and output frequency
+        power_error = abs(float(power_max) - float(powe))# Calculating difference between input and output power
+        frequency_th = 0.1  # frequency threshold in terms of percentage of input frequency
+        power_th = 1  # power threshold in dBm
+        if (error_value < (frequency_th * cf)) and (abs(power_error) < power_th):  # Condition to conclude the test result
+            devicePrintCmd.msg_user.set('Test pass for power level {0} dBm'.format(powe))
+            devicePrintCmd.Print()
         else:
-            print("Test fail  for Frequency = {0} MHz".format(cf))
-            
-    print(f"Keep Center frequency at 1GHz with Frequency = {freq_query}Hz span and verify the signal on the screen for Frequency = {freq_query}, Power ={power_query}")
-    print(" Press on the Peak search button on Spectrum analyzer and Note down the frequency and power of desired signal")
-    print("Press enter for next power test")
-    input()
-    
-# disconnect instrument
+            print('Test Fail for power level {0} dBm'.format(powe))
+            devicePrintCmd.Print()
+            devicePrintCmd.msg_user.set('Test Fail for power level {0} dBm'.format(powe))
+            devicePrintCmd.Print()
+
+        devicePrintCmd.msg_user.set('Press enter for next power level test')
+        devicePrintCmd.Print()
+        input()
+
+# SECTION 5 - Closing the instruments
 Lucid_functions.disconnect_lucid(config.handle)
 ###END OF SCRIPT###
