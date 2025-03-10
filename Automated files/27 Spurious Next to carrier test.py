767369beef79875b46e760f8fd2ae20d45324398
shivani <shivani@taborelec.com>
2025-03-10 13:04:43 +0530

    26-27 Spurious tests

--- Automated files/27 Spurious Next to carrier test.py
+++ Automated files/27 Spurious Next to carrier test.py
@@ -1,4 +1,4 @@
-print("<DESCRIPTION> Test description- This test verifies, there is no spurious present in-band (between start and stop frequency ie span ) of the carrier above the defined error limit ie -60dB</DESCRIPTION>")
+print("<DESCRIPTION> Test description- This test verifies, there is no spurious present near the carrier frequency (between start and stop frequency ie span ) of the carrier above the defined error limit ie -60dB</DESCRIPTION>")
 ###########################
 ###START OF SCRIPT###
 # SECTION 0 - Import the required libraries
@@ -29,18 +29,16 @@ if config.spectrum:
         spectrum_methods.set_span_freq(200, spectrum_analyzer)  # step 2) set span to 200MHz
         threshold = -60 + power_in_dBm
         spectrum_methods.set_peak_threshold(threshold, spectrum_analyzer)
-        spectrum_methods.set_resolution_bandwidth(30000, spectrum_analyzer)
+        spectrum_methods.set_resolution_bandwidth(100000, spectrum_analyzer)
 
-# SECTION 2- Defining parameters and generate signal form LUCIDX
-frequency_list =[1000,2000,3500,4000,550,2300,1500,3500,4000,3500,4000,550,2300,1500,3500,4000]
-start_freq_tb = [500,1500,2400,2400,3400,3900,4900,5900,5900,6900,6900,7900,8900,9900,10900,10900]
-stop_freq_tb = [1500,2400,3400,3400,3900,4900,5900,6900,6900,7900,7900,8900,9900,10900,11900,11900]
-output = np.zeros(len(frequency_list)) # To store the output frequency from spectrum analyzer (optional step)
-power_in_dBm = 5# power level required for the test in dBm
-for i in range(len(frequency_list)):
-    freq_in = frequency_list[i]
+####################################################################
+# Input Parameters
+frequency_list =list(np.arange(20,110,10))#+list(np.arange(150,1000,50)))+list(np.arange(1000,3000,500))#list(np.arange(1000,12000,1000))#+list(np.arange(150,1050,50))+list(np.arange(1250,12250,250)) # list of frequencies to be tested
+frequnecy_list = config.frequencies
+
+for freq_in in frequency_list: # iterating over each frequency (main loop)
     # continous wave generation
-    freq_query, power_query = SignalGeneration.continous_wave_generation(freq_in, power_in_dBm)  # continous signal generation
+    freq_query, power_query = SignalGeneration.continous_wave_generation(freq_in,power_in_dBm)  # continous signal generation
     devicePrintCmd.msg_gui.set(f'freq={freq_query}::p0.00::n0.00,pow={power_query}::p0.00::n0.00')
     devicePrintCmd.Print()
     
@@ -54,21 +52,30 @@ for i in range(len(frequency_list)):
         error_value = abs(float(freq_out) - freq_in)  # Calculating difference between input and output frequency
         power_error = abs(float(power_max) - float(power_in_dBm))  # Calculating difference between input and output power
         frequency_th = 0.1  # frequency threshold in terms of percentage of input frequency
-        power_th = 4 # power threshold in dBm
-        # a=error_value < (frequency_th * freq_in)
-        # b=power_error < power_th
-        # print(a,b)
-        if (error_value < (frequency_th * freq_in)) and (power_error < power_th):  # Condition to conclude the test result
-            spectrum_methods.set_start_freq(start_freq_tb[i],spectrum_analyzer)
-            spectrum_methods.set_stop_freq(stop_freq_tb[i],spectrum_analyzer)
-            
-            freq_left, power_left = spectrum_methods.get_left_peak(cf,spectrum_analyzer)
+        power_th = 4  # power threshold in dBm
+        a=error_value < (frequency_th * freq_in)
+        b=power_error < power_th
+        print(a,b)
+        if (error_value < (frequency_th * freq_in)) and (power_error < power_th):
+            if cf <= 20 or cf <= 100:
+                step_size = 10
+                span_sa = 20
+            elif cf <= 150 or cf <= 1000:
+                step_size = 50
+                span_sa = 30
+            elif cf <= 1250 or cf <= 12000:
+                step_size = 50
+                span_sa = 40
+            else:
+                continue
+            spectrum_methods.set_span_freq(span_sa, spectrum_analyzer)  # step 2) set span to 200MHz
+          
+            freq_left, power_left = spectrum_methods.get_left_peak(cf, spectrum_analyzer)
             devicePrintResp.msg_gui.set(f'freq={freq_left}::p0.00::n0.00,pow={power_left}::p0.00::n0.00')
             devicePrintResp.Print()
-            freq_right, power_right = spectrum_methods.get_right_peak(cf,spectrum_analyzer)
+            freq_right, power_right = spectrum_methods.get_right_peak(cf, spectrum_analyzer)
             devicePrintResp.msg_gui.set(f'freq={freq_right}::p0.00::n0.00,pow={power_right}::p0.00::n0.00')
             devicePrintResp.Print()
-            
             if freq_left == freq_right:
                 devicePrintCmd.msg_user.set(f'Spurious test pass for Frequency = {freq_in} MHz')
                 devicePrintCmd.Print()
@@ -88,4 +95,3 @@ for i in range(len(frequency_list)):
 Lucid_functions.disconnect_lucid(config.handle)
 ###END OF SCRIPT###
 
-    
